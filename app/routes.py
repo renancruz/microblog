a788cc4d6e598a1e2b33b5307ca18f936edb93d6
renan.frcr@gmail.com <renancruz>
2022-05-11 14:11:47 -0300

    Utilizando metodos http

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template
+from flask import request
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
-  nome = "Renan"
+@app.route('/index', defaults={"nome":"usuário"})
+@app.route('/index/<nome>')
+def index(nome):
   dados = {"profissao": "Desenvolvedor", "canal": "Renan Cruz"}
   return render_template('index.html', nome=nome, dados=dados)
 
@@ -12,3 +13,18 @@ def index():
 @app.route('/contato')
 def contato():
   return render_template('contato.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+  #GET
+  # usuario = request.args.get('usuario')
+  # senha = request.args.get('senha')
+
+  #POST
+  usuario = request.form.get('usuario')
+  senha = request.form.get('senha')
+  return "usuario: {} e senha: {}".format(usuario, senha)
